2526cbaafdf60028fc9e3ae299fecfe7912b635c
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-05-10 16:50:49 +0200

    change default window size

--- wezterm.lua
+++ wezterm.lua
@@ -4,6 +4,16 @@ local config = {}
 config.color_scheme = "Oxocarbon Dark"
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.initial_rows = 64
+config.initial_cols = 236
+
+config.keys = {
+  {
+    key = "0",
+    mods = "CTRL",
+    action = wezterm.action.ResetFontAndWindowSize,
+  },
+}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = "SteadyBlock"
